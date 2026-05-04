b54821f8ae1cd574f340ddaaaf180f0c8a0fa049
NilaAyu1611 <nilaayucw@gmail.com>
2026-05-04 22:53:14 +0700

    feat: set up Express server with CORS and route handling for authentication and lapangan

---
+++ ta-lap-be/src/index.js
@@ -0,0 +1,51 @@
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = new PrismaClient();
+
+// async function test() {
+//   try {
+//     const users = await prisma.user.findMany();
+//     console.log(users);
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
+
+// test();
+
+
+// import express from "express";
+// import { PrismaClient } from "@prisma/client";
+
+// const app = express();
+// const prisma = new PrismaClient();
+
+// app.get("/", async (req, res) => {
+//   const users = await prisma.user.findMany();
+//   res.json(users);
+// });
+
+// app.listen(3002, () => {
+//   console.log("Server jalan di http://localhost:3002");
+// });
+
+import express from "express";
+import cors from "cors";
+
+import authRoutes from "./routes/authRoutes.js";
+import lapanganRoutes from "./routes/lapanganRoutes.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+app.use("/lapangan", lapanganRoutes);
+
+app.listen(3002, () => {
+  console.log("Server jalan di port 3002");
+});
