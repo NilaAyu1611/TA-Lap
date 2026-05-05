27615f05142dc72e1a166641f4be7709985fa4d6
NilaAyu1611 <nilaayucw@gmail.com>
2026-05-05 20:52:20 +0700

    feat: enhance roleMiddleware to check for user authentication and provide localized error messages

--- ta-lap-be/src/middlewares/roleMiddleware.js
+++ ta-lap-be/src/middlewares/roleMiddleware.js
@@ -1,10 +1,29 @@
+// export const roleMiddleware = (...roles) => {
+//   return (req, res, next) => {
+//     if (!roles.includes(req.user.role)) {
+//       return res.status(403).json({
+//         message: "Forbidden"
+//       });
+//     }
+//     next();
+//   };
+// };
+
+
 export const roleMiddleware = (...roles) => {
   return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      return res.status(403).json({
-        message: "Forbidden"
+    if (!req.user) {
+      return res.status(401).json({
+        message: "Unauthorized"
       });
     }
+
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({
+        message: "Akses ditolak"
+      });
+    }
+
     next();
   };
 };
