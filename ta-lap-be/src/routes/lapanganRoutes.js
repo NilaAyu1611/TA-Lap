3428e71b2ccff2c1ef9c633aa56f0537cea9ee15
NilaAyu1611 <nilaayucw@gmail.com>
2026-05-05 20:55:39 +0700

    feat: update lapanganRoutes to include update and delete operations with role-based access control

--- ta-lap-be/src/routes/lapanganRoutes.js
+++ ta-lap-be/src/routes/lapanganRoutes.js
@@ -1,21 +1,64 @@
+// import express from "express";
+// import {
+//   getAllLapangan,
+//   createLapangan
+// } from "../controllers/lapanganController.js";
+
+// import { authMiddleware } from "../middlewares/authMiddleware.js";
+// import { roleMiddleware } from "../middlewares/roleMiddleware.js";
+
+// const router = express.Router();
+
+// router.get("/", getAllLapangan);
+
+// router.post(
+//   "/",
+//   authMiddleware,
+//   roleMiddleware("owner", "admin"),
+//   createLapangan
+// );
+
+// export default router;
+
+
 import express from "express";
 import {
   getAllLapangan,
-  createLapangan
+  createLapangan,
+  updateLapangan,
+  deleteLapangan,
 } from "../controllers/lapanganController.js";
 
-import { authMiddleware } from "../middleware/authMiddleware.js";
-import { roleMiddleware } from "../middleware/roleMiddleware.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { roleMiddleware } from "../middlewares/roleMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", getAllLapangan);
+// GET → semua role boleh
+router.get("/", authMiddleware, getAllLapangan);
 
+// CREATE → owner & admin
 router.post(
   "/",
   authMiddleware,
   roleMiddleware("owner", "admin"),
   createLapangan
+);
+
+// UPDATE
+router.put(
+  "/:id",
+  authMiddleware,
+  roleMiddleware("owner", "admin"),
+  updateLapangan
+);
+
+// DELETE
+router.delete(
+  "/:id",
+  authMiddleware,
+  roleMiddleware("owner", "admin"),
+  deleteLapangan
 );
 
 export default router;
